9a3c998bc31aeb4f2503ba459bc2062155b7f33e
starking <star123joker@gmail>
2021-12-08 11:31:11 +0800

    做完異動腳本

--- topic/models.py
+++ topic/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     subject = models.CharField('討論主題', max_length=255)
     content = models.TextField('內文')
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #作者連結到User
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #作者連結到User #CASCADE:當帳號被刪除時，也會將 Topic 這個資料模型裡所有參考到該帳號的紀錄一起刪除。
     created = models.DateTimeField('建立時間', auto_now_add=True) #自動填時間
     replied = models.DateTimeField('回覆時間', null=True, blank=True)
 
     def __str__(self):
         return "{}: {}".format(self.author, self.subject)
+#superuser ann password:123456789
